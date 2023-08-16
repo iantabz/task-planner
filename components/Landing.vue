c53aa54c08249d343828db448a3f9575ecc010f3
ianmark <ianmark.tabz@gmail.com>
2023-08-16 14:51:54 +0800

    added new landing page

---
+++ components/Landing.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container px-0 py-auto d-flex flex-column justify-content-center align-items-center">
+        <div class="hero-section__title">
+            <h1>Welcome to Task Planner</h1>
+        </div>
+        <div class="hero-section__description text-center">
+            <p>Get things done with your day, a list that refreshes every day.</p>
+        </div>
+        <InputTask :linktoTasks="true" />
+    </div>
+</template>
+  
+<script>
+export default {};
+</script>
+  
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container px-0 py-auto d-flex flex-column justify-content-center align-items-center">
+    <Landing />
+  </div>
+</template>
+
+<script>
+export default {
+  transition: 'home'
+};
+</script>
+
+<style>
+.home-enter-active,
+.home-leave-active {
+  transition: opacity .5s;
+}
+
+.home-enter,
+.home-leave-active {
+  opacity: 0;
+}</style>
