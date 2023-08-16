3c38c6a6c98b56248cd679a6d2d6adea3b4e0428
ianmark <ianmark.tabz@gmail.com>
2023-08-16 14:07:05 +0800

    Config update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,25 @@
+/** @format */
+
 module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true,
+    node: true
   },
   parserOptions: {
-    parser: "@babel/eslint-parser",
-    requireConfigFile: false,
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false
   },
   extends: [
-    "@nuxtjs",
-    "plugin:nuxt/recommended",
-    "prettier",
-    "plugin:cypress/recommended",
+    '@nuxtjs',
+    'plugin:nuxt/recommended',
+    'prettier',
+    'plugin:cypress/recommended'
   ],
   plugins: [],
   // add your custom rules here
   rules: {
-    "vue/multi-word-component-names": "off",
-    "vue/no-v-html": "off",
-  },
-};
+    'vue/multi-word-component-names': 'off',
+    'vue/no-v-html': 'off'
+  }
+}
