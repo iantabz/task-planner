793494cdb4e32e7f390575f9600924315b241b71
ianmark <ianmark.tabz@gmail.com>
2023-08-16 14:40:59 +0800

    config update

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,19 +1,17 @@
-/** @format */
-
 module.exports = {
-  customSyntax: 'postcss-html',
+  customSyntax: "postcss-html",
   extends: [
-    'stylelint-config-standard',
-    'stylelint-config-recommended-vue',
-    'stylelint-config-prettier'
+    "stylelint-config-standard",
+    "stylelint-config-recommended-vue",
+    "stylelint-config-prettier",
   ],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
   rules: {
-    'property-no-vendor-prefix': null,
-    'selector-pseudo-element-colon-notation': null,
-    'selector-class-pattern': null,
-    'rule-empty-line-before': null,
-    'font-family-name-quotes': null
-  }
-}
+    "property-no-vendor-prefix": null,
+    "selector-pseudo-element-colon-notation": null,
+    "selector-class-pattern": null,
+    "rule-empty-line-before": null,
+    "font-family-name-quotes": null,
+  },
+};
